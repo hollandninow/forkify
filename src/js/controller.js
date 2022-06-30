1d8e5c9efd2b6ca31a704c28d61c83cc1ae4c5af
hollandninow <holland.ninow@gmail.com>
2022-06-30 08:20:29 -0700

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -131,6 +131,10 @@ const clearBookmarks = function () {
 };
 // clearBookmarks();
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -139,6 +143,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
